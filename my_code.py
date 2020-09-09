730807688386d9f654d371805fb70e3370b70747
23aartale <23aartale@athenian.org>
2020-09-09 20:23:45 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -2,3 +2,10 @@
 #  
 
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
+
+
+name = input("Whats your name ")
+print("Nice to meet you " + name)
+
+job = input("What do you want to be when you grow up? ")
+print ( name + " you will become a " + job)
